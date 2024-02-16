02c37c2ea8249ce241d5aa121532909b9f49ce56
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2024-02-16 17:39:56 +0530

    Creating the redux store

---
+++ reducers/auth.reducer.js
@@ -0,0 +1,30 @@
+import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    loading: true,
+};
+
+export default function (state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                ...payload,
+                loading: false,
+            };
+        case LOGIN_FAIL:
+        case LOGOUT:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+}
